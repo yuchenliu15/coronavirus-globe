5fe46ae508bdb8fb3e47c3133ec18727a6b6339c
Yuchen Liu <yl6448@nyu.edu>
2020-05-11 17:56:38 -0700

    add database

---
+++ operations/database.js
@@ -0,0 +1,32 @@
+const sqlite3 = require('sqlite3').verbose();
+const dbName = 'location.sql';
+const db = new sqlite3.Database(dbName);
+
+db.serialize(() => {
+    const sql = `
+        CREATE TABLE IF NOT EXIST locations
+        (lat REAL PRIMARY KEY, lng REAL, size INTEGER)
+    `;
+    db.run(sql);
+});
+
+class Location {
+    
+    static all(callback) {
+        db.all('SELECT * FROM locations', callback);
+    }
+
+    static create(data, callback) {
+        const sql = 'INSERT INTO locations(lat,lng,size) VALUES (?,?,?)';
+        db.run(sql, data.lat, data.lng, data.size, callback);
+    }
+
+    static update(data, callback) {
+        const sql = 'UPDATE locations SET size = ? WHERE id = ?';
+        db.run(sql, data.size, data.lat, callback);
+    }
+
+}
+
+module.exports = db;
+module.exports.Location = Location; 
