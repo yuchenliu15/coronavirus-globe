dd80cde86867c4947db7867b4a10895d13bd626a
Yuchen Liu <yl6448@nyu.edu>
2020-05-12 18:05:18 -0700

    remove liquid, will choose other templates

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const getVirusData = require('./operations/data');
 
 app.get('/', (req, res) => {
-    getVirusData()
-        .then(result => {
-            res.send(result);
-        });
+
+    // getVirusData()
+    //     .then(result => {
+    //         res.send(result);
+    //     });
 });
 
 app.listen(process.env.PORT, () => {
