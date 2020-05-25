83c49a23738a3bab6ae8d496136ae1c683a8846c
Yuchen Liu <yl6448@nyu.edu>
2020-05-24 22:29:42 -0700

    display globe

--- public/js/globeApp.js
+++ public/js/globeApp.js
@@ -1,8 +1,45 @@
-fetch('http://localhost:3000/data')
+fetch('/data')
     .then(res=>res.json())
     .then(data => {
-        console.log('HAHHAHA');
         console.log(data)
+        const Globe = new ThreeGlobe()
+            .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+            .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+            .pointsData(data)
+            .pointAltitude('size')
+            .pointColor(() => '#2980b9');
+
+
+        // Setup renderer
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('globeViz').appendChild(renderer.domElement);
+
+        // Setup scene
+        const scene = new THREE.Scene();
+        scene.add(Globe);
+        scene.add(new THREE.AmbientLight(0xbbbbbb));
+        scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
+
+        // Setup camera
+        const camera = new THREE.PerspectiveCamera();
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        camera.position.z = 500;
+
+        // Add camera controls
+        const tbControls = new TrackballControls(camera, renderer.domElement);
+        tbControls.minDistance = 101;
+        tbControls.rotateSpeed = 5;
+        tbControls.zoomSpeed = 0.8;
+
+        // Kick-off renderer
+        (function animate() { // IIFE
+            // Frame cycle
+            tbControls.update();
+            renderer.render(scene, camera);
+            requestAnimationFrame(animate);
+        })();
     });
 
 
@@ -53,42 +90,4 @@ fetch('http://localhost:3000/data')
 //         globe_data = globe_data.filter(x => x !== undefined)
 //         console.log(globe_data);
 
-//         const Globe = new ThreeGlobe()
-//             .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-//             .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-//             .pointsData(globe_data)
-//             .pointAltitude('size')
-//             .pointColor(() => '#2980b9');
-
-
-//         // Setup renderer
-//         const renderer = new THREE.WebGLRenderer();
-//         renderer.setSize(window.innerWidth, window.innerHeight);
-//         document.getElementById('globeViz').appendChild(renderer.domElement);
-
-//         // Setup scene
-//         const scene = new THREE.Scene();
-//         scene.add(Globe);
-//         scene.add(new THREE.AmbientLight(0xbbbbbb));
-//         scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
-
-//         // Setup camera
-//         const camera = new THREE.PerspectiveCamera();
-//         camera.aspect = window.innerWidth / window.innerHeight;
-//         camera.updateProjectionMatrix();
-//         camera.position.z = 500;
-
-//         // Add camera controls
-//         const tbControls = new TrackballControls(camera, renderer.domElement);
-//         tbControls.minDistance = 101;
-//         tbControls.rotateSpeed = 5;
-//         tbControls.zoomSpeed = 0.8;
-
-//         // Kick-off renderer
-//         (function animate() { // IIFE
-//             // Frame cycle
-//             tbControls.update();
-//             renderer.render(scene, camera);
-//             requestAnimationFrame(animate);
-//         })();
 //     });
