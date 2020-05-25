b3f6ec8ea33aaee8ec28fd0545fda97b3cdb9a97
Yuchen Liu <yl6448@nyu.edu>
2020-05-24 22:22:04 -0700

    client side js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 require('dotenv').config();
 const getVirusData = require('./operations/data');
+const path = require('path');
 const Location = require('./operations/database').Location;
 
 const executeTime = 1440 * 60 * 1000;
@@ -20,7 +22,15 @@ const updateDB = async () => {
 updateDB();
 setInterval(updateDB, executeTime);
 
+app.use('/public', express.static(path.join(__dirname + '/public')));
+
 app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '/index.html'));
+
+});
+
+app.get('/data', (req, res) => {
 
     Location.all((error,data)  => {
         if(error) console.error(error)
