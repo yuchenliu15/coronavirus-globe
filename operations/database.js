591960d3eecacb2764787d2c167e0ca852ba4804
yuchen <yl6448@nyu.edu>
2020-06-26 21:41:28 -0700

    add knex

--- operations/database.js
+++ operations/database.js
@@ -1,26 +1,33 @@
-const sqlite3 = require('sqlite3').verbose();
-const dbName = 'location.sql';
-const db = new sqlite3.Database(dbName);
+const knex = require('knex');
 
-db.serialize(() => {
-    const sql = `
-        CREATE TABLE IF NOT EXISTS locations
-        (name TEXT PRIMARY KEY, lat REAL, lng REAL, size FLOAT)
-    `;
-    db.run(sql);
+const db = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: 'location.sql'
+    },
+    useNullAsDefault: true
 });
 
-class Location {
-    
-    static all(callback) {
-        db.all('SELECT * FROM locations', callback);
+module.exports = () => {
+    return db.schema.createSchemaIfNotExists('locations', table => {
+        table.text('name').primary();
+        table.real('lat');
+        table.real('lng');
+        table.float('size');
+    })
+};
+module.exports.Location = {
+    all(callback) {
+        return db('locations').select().asCallback(callback);
+    },
+    create(data, callback) {
+        return db('locations').insert({
+            name: data.name,
+            lat: data.lat,
+            lng: data.lng,
+            size: data.size
+        })
+        .asCallback(callback);
     }
 
-    static create(data, callback) {
-        const sql = 'REPLACE INTO locations(name,lat,lng,size) VALUES (?,?,?,?)';
-        db.run(sql, data.name, data.lat, data.lng, data.size, callback);
-    }
-}
-
-module.exports = db;
-module.exports.Location = Location;
+};
